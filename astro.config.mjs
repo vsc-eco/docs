c428d345b2fe453df39b6586049f02b726718a13
redsnahoj <97570183+redsnahoj@users.noreply.github.com>
2025-11-01 13:46:11 -0400

    Add lang Spanish

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,21 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'VSC Documentation',
+      title: {
+        en: 'VSC Documentation',
+        'es': 'VSC Documentación',
+      },
+      defaultLocale: 'root',
+      locales: {
+        root: {
+          label: 'English',
+          lang: 'en',
+        },
+        'es': {
+          label: 'Español',
+          lang: 'es',
+        },
+      },
       social: [
         { icon: 'github', label: 'GitHub', href: 'https://github.com/vsc-eco' }
       ],
